01b4ce1369d0f723eda1195ce9b1f9ed2944f8c0
tom1236908745 <tom1233908745@gmail.com>
2022-04-25 16:46:49 +0900

    add: ヘッダーの作成

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="siteHeader">
+    <div>都道府県別の総人口推移</div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.siteHeader {
+  line-height: 5rem;
+  text-align: center;
+  background-color: #85cef0;
+}
+.siteHeader div {
+  font-size: 1.5rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Header />
     <Loading v-show="loading === true" />
     <PrefCheckBoxes v-show="loading === false" />
   </div>
@@ -9,9 +10,10 @@
 import Vue from "vue";
 import PrefCheckBoxes from "~/components/PrefCheckBoxes.vue";
 import Loading from "../components/Loading.vue";
+import Header from "~/components/Header.vue";
 
 export default Vue.extend({
-  components: { PrefCheckBoxes, Loading },
+  components: { PrefCheckBoxes, Loading, Header },
   computed: {
     loading() {
       return this.$store.getters.loading;
