86a238a9efb3259c5b40c2117defe6c34fb1f96e
tom1236908745 <tom1233908745@gmail.com>
2022-04-25 16:49:35 +0900

    Revert "add: ヘッダーの作成"
    This reverts commit 01b4ce1369d0f723eda1195ce9b1f9ed2944f8c0.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <Header />
     <Loading v-show="loading === true" />
     <PrefCheckBoxes v-show="loading === false" />
   </div>
@@ -10,10 +9,9 @@
 import Vue from "vue";
 import PrefCheckBoxes from "~/components/PrefCheckBoxes.vue";
 import Loading from "../components/Loading.vue";
-import Header from "~/components/Header.vue";
 
 export default Vue.extend({
-  components: { PrefCheckBoxes, Loading, Header },
+  components: { PrefCheckBoxes, Loading },
   computed: {
     loading() {
       return this.$store.getters.loading;
